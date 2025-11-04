5d04fe4cc71d6311f261178cff7a414b9e1eb1c7
skurzyp-arianelabs <stanislaw.kurzyp@arianelabs.com>
2025-11-04 10:20:59 +0100

    feat: add delete account functionality
    Signed-off-by: skurzyp-arianelabs <stanislaw.kurzyp@arianelabs.com>

--- python/hedera_agent_kit_py/plugins/core_account_plugin/__init__.py
+++ python/hedera_agent_kit_py/plugins/core_account_plugin/__init__.py
@@ -1,4 +1,5 @@
 from hedera_agent_kit_py.shared.plugin import Plugin
+from .delete_account import DeleteAccountTool, DELETE_ACCOUNT_TOOL
 from .transfer_hbar import TransferHbarTool, TRANSFER_HBAR_TOOL
 
 core_account_plugin = Plugin(
@@ -7,15 +8,18 @@ core_account_plugin = Plugin(
     description="A plugin for the Hedera Account Service",
     tools=lambda context: [
         TransferHbarTool(context),
+        DeleteAccountTool(context),
     ],
 )
 
 core_account_plugin_tool_names = {
     TRANSFER_HBAR_TOOL,
+    DELETE_ACCOUNT_TOOL
 }
 
 __all__ = [
     "core_account_plugin",
     "core_account_plugin_tool_names",
     "TransferHbarTool",
+    "DeleteAccountTool",
 ]
