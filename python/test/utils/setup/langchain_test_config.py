dd26df4dc901fed456a413780f204e8e0afd3395
skurzyp <kurzyp.st@gmail.com>
2025-11-19 11:24:06 +0100

    chore: resolve conflicts
    Signed-off-by: skurzyp <kurzyp.st@gmail.com>

--- python/test/utils/setup/langchain_test_config.py
+++ python/test/utils/setup/langchain_test_config.py
@@ -23,6 +23,7 @@ from hedera_agent_kit_py.plugins.core_evm_plugin import (
     core_evm_plugin_tool_names,
     core_evm_plugin,
 )
+from hedera_agent_kit_py.plugins.core_misc_query_plugin import (core_misc_query_plugin_tool_names, core_misc_query_plugin)
 from hedera_agent_kit_py.plugins.core_transaction_query_plugin import (
     core_transaction_query_plugin, core_transaction_query_plugin_tool_names,
 )
