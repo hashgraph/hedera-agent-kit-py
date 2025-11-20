ffc059253beeade762613f178705a3aaea8aec6d
skurzyp-blockydevs <stanislaw.kurzyp@blockydevs.com>
2025-11-20 17:57:28 +0100

    feat: implement transfer hbar with allowance tool. Create unit tests and tool matching tests
    Signed-off-by: skurzyp-blockydevs <stanislaw.kurzyp@blockydevs.com>

--- python/test/utils/setup/langchain_test_config.py
+++ python/test/utils/setup/langchain_test_config.py
@@ -39,6 +39,9 @@ DELETE_ACCOUNT_TOOL = core_account_plugin_tool_names["DELETE_ACCOUNT_TOOL"]
 CREATE_ACCOUNT_TOOL = core_account_plugin_tool_names["CREATE_ACCOUNT_TOOL"]
 TRANSFER_HBAR_TOOL = core_account_plugin_tool_names["TRANSFER_HBAR_TOOL"]
 UPDATE_ACCOUNT_TOOL = core_account_plugin_tool_names["UPDATE_ACCOUNT_TOOL"]
+TRANSFER_HBAR_WITH_ALLOWANCE_TOOL = core_account_plugin_tool_names[
+    "TRANSFER_HBAR_WITH_ALLOWANCE_TOOL"
+]
 CREATE_TOPIC_TOOL = core_consensus_plugin_tool_names["CREATE_TOPIC_TOOL"]
 DELETE_TOPIC_TOOL = core_consensus_plugin_tool_names["DELETE_TOPIC_TOOL"]
 GET_HBAR_BALANCE_QUERY_TOOL = core_account_query_plugin_tool_names[
@@ -104,6 +107,7 @@ TOOLKIT_OPTIONS: LangchainTestOptions = LangchainTestOptions(
         SUBMIT_TOPIC_MESSAGE_TOOL,
         GET_ACCOUNT_QUERY_TOOL,
         GET_TRANSACTION_RECORD_QUERY_TOOL,
+        TRANSFER_HBAR_WITH_ALLOWANCE_TOOL,
     ],
     plugins=[
         core_account_plugin,
