8008910e85434683b9ae9824df470ad6e5626330
skurzyp-blockydevs <stanislaw.kurzyp@blockydevs.com>
2025-12-15 11:27:39 +0100

    chore: format code
    Signed-off-by: skurzyp-blockydevs <stanislaw.kurzyp@blockydevs.com>

--- python/hedera_agent_kit/plugins/core_consensus_query_plugin/__init__.py
+++ python/hedera_agent_kit/plugins/core_consensus_query_plugin/__init__.py
@@ -3,8 +3,10 @@ from hedera_agent_kit.plugins.core_consensus_query_plugin.get_topic_info_query i
     GET_TOPIC_INFO_QUERY_TOOL,
 )
 
-from hedera_agent_kit.plugins.core_consensus_query_plugin.get_topic_messages_query import (GetTopicMessagesQueryTool,
-    GET_TOPIC_MESSAGES_QUERY_TOOL)
+from hedera_agent_kit.plugins.core_consensus_query_plugin.get_topic_messages_query import (
+    GetTopicMessagesQueryTool,
+    GET_TOPIC_MESSAGES_QUERY_TOOL,
+)
 from hedera_agent_kit.shared.plugin import Plugin
 
 core_consensus_query_plugin = Plugin(
@@ -13,18 +15,18 @@ core_consensus_query_plugin = Plugin(
     description="A plugin for the Hedera Consensus Service (HCS)",
     tools=lambda context: [
         GetTopicInfoQueryTool(context),
-        GetTopicMessagesQueryTool(context)
+        GetTopicMessagesQueryTool(context),
     ],
 )
 
 core_consensus_query_plugin_tool_names = {
     "GET_TOPIC_INFO_QUERY_TOOL": GET_TOPIC_INFO_QUERY_TOOL,
-    "GET_TOPIC_MESSAGES_QUERY_TOOL": GET_TOPIC_MESSAGES_QUERY_TOOL
+    "GET_TOPIC_MESSAGES_QUERY_TOOL": GET_TOPIC_MESSAGES_QUERY_TOOL,
 }
 
 __all__ = [
     "core_consensus_query_plugin",
     "core_consensus_query_plugin_tool_names",
     "GetTopicInfoQueryTool",
-    "GetTopicMessagesQueryTool"
+    "GetTopicMessagesQueryTool",
 ]
